91ed082a8f1c40d045927bb8463f0ccb1b024b54
Ehab Khaireldin <ehab.khaireldin@onemeter.com>
2018-05-19 21:04:42 +0200

    un-gitignoring config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const mongoUriParser = require('mongodb-uri');
+const winston = require('winston');
+const isDev = process.env.NODE_ENV == 'development' || false;
+const isTesting = process.env.NODE_ENV == 'test' || false;
+
+const mongoUri =
+  isDev || isTesting
+    ? `mongodb://127.0.0.1:27017/testingMongoCrud`
+    : process.env.MONGO_URI;
+
+module.exports = {
+  mongoConf: {
+    uri: mongoUri,
+    dbName: mongoUriParser.parse(mongoUri).database,
+  },
+  expressConf: {
+    port: process.env.PORT || 3000,
+  },
+};
